3339a9056eeda6596cf4af4a16de299a30992645
codekunoichi <giri.rumpa2017@gmail.com>
2021-04-17 03:08:24 -0700

    Exploring route changes from hello to goodbye on root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "¡Hola, mundo!, Hellow World!"
+    end
+
+    def goodbye
+        render html: "Goodye, See you soon!"
+    end
 end
